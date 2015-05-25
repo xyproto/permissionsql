5585b478f70bea8a3c489bdf260931c953dddd05
Alexander Rødseth <rodseth@gmail.com>
2015-05-25 10:14:01 +0200

    Updated the examples

--- examples/macaron/main.go
+++ examples/macaron/main.go
@@ -13,7 +13,7 @@ func main() {
 	m := macaron.Classic()
 
 	// New permissions middleware
-	perm := permissions.New()
+	perm := permissionsql.New()
 
 	// Blank slate, no default permissions
 	//perm.Clear()
